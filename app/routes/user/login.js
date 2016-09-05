2e09da35222451d9175d960494f99a97a6933490
Cristian Buffa <bufface@gmail.com>
2016-09-05 11:37:43 -0300

    Conditional rendering navbar when loggin success

--- app/routes/user/login.js
+++ app/routes/user/login.js
@@ -2,19 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    let session = this.get("session").fetch().catch(function() {});
-    console.log(session);
-    return session;
+    return this.get("session").fetch().catch(function() {});
   },
 
   actions: {
     loginWithSocialMedia: function(provider) {
-      console.log(provider);
       this.get("session").open("firebase", { provider: provider}).then(function(data) {
         console.log(data.currentUser);
       });
     },
-    signOut: function() {
+    logOut: function() {
       this.get("session").close();
     }
   }
